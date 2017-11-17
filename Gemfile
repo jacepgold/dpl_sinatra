61ece7b79dcf8f0e7f6d044a7bd21258f878d024
Jace P. Gold <Jace@JaceGold.com>
2017-11-17 14:28:05 -0700

    initial upload

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+ruby '2.3.1'
+
+# Web Framework
+gem 'sinatra', '~> 2.0.0'
+# Debugging - call binding.pry anywhere in code
+gem 'pry', '~> 0.11.3'
+# Web server
+gem 'thin', '~> 1.7.2'
---
+++ server.rb
@@ -0,0 +1,47 @@
+require 'sinatra'
+require 'pry'
+
+# HTTP VERBS
+# GET - fetches data from the server
+# POST - creates data on the server
+# PUT / PATCH - updates data on the server
+# DELETE - removes data from the server
+
+# CRUD
+# CREATE - POST
+# READ - GET
+# UPDATE - PUT / PATCH
+# DELETE - DELETE
+
+# Root Route
+get '/' do
+  "Hello World!"
+end 
+
+# Example localhost:4567/hello/jake
+# :name is a dynamic placeholder
+# URL Params
+get '/hello/:name' do
+  "Hello #{params[:name]}"
+end
+
+# Example: localhost:4567/name/jake/sport/snowboarding
+get '/name/:name/sport/:sport' do
+  "Hello #{params[:name]}, Your sport is: #{params[:sport]}"
+end
+
+# Example: localhost:4567/code?language=ruby&experience=expert
+get '/code' do
+  @language = params[:language]
+  @experience = params[:experience]
+  erb :code
+end
+
+get '/people' do
+  @name = params[:name]
+  erb :people
+end
+
+get '/blocks' do
+  erb :blocks
+end
